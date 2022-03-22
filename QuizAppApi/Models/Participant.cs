41eef01c5f07aea2df26f23d1721ee75fe691465
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-03-22 23:03:03 +0200

    Add new model Participant

---
+++ QuizAppApi/Models/Participant.cs
@@ -0,0 +1,22 @@
+
+namespace QuizApi.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Participant
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Email { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Name { get; set; }
+
+        public int Score { get; set; }
+
+        public int CompletionTime { get; set; }
+    }
+}
