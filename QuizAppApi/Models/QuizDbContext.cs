6629b8fe844b481b6a96e9959015aacd9385b42d
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-03-23 00:17:34 +0200

    Configure database

---
+++ QuizAppApi/Models/QuizDbContext.cs
@@ -0,0 +1,13 @@
+﻿
+namespace QuizApi.Models
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class QuizDbContext : DbContext
+    {
+        public QuizDbContext(DbContextOptions<QuizDbContext> options): base(options) { }
+
+        public DbSet<Participant> Participants { get; set; }
+        public DbSet<Question> Questions { get; set; }
+    }
+}
