51268f4161804d1cab27923abb4ea1a001862ad4
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-03-29 00:31:09 +0300

    Integrate createAPIEndpoint in Quiz JSX

--- quiz-client/src/components/Quiz.js
+++ quiz-client/src/components/Quiz.js
@@ -1,10 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { createAPIEndpoint, ENDPOINTS } from '../api';
 import useStateContext from '../hooks/useStateContext';
 
 export default function Quiz() {
 
-  useEffect(() => {
+  const [ qns, setQns ] = useState([]);
 
+  useEffect(() => {
+    createAPIEndpoint(ENDPOINTS.question)
+      .fetch()
+      .then(res => {
+          setQns(res.data);
+          console.log(res.data);
+      })
+      .catch(err => {console.log("Error: " + err);})
   }, [])
 
   return (
