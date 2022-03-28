6d4a66c6045f959414c803bd958d9c18fb08f13a
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-03-28 17:22:52 +0300

    Fix routing imports

--- quiz-client/src/App.js
+++ quiz-client/src/App.js
@@ -1,16 +1,18 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
+import Quiz from './components/Login';
+import Result from './components/Login';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path="/" element="{<Login />}" />
-        <Route path="/quiz" element="{<Quiz />}" />
-        <Route path="/result" element="{<Result />}" />
+        <Route path="/" element={<Login />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/result" element={<Result />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   )
 }
 
