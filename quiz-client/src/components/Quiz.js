6c769af0b33214c260a3e3b8e53d2d3a6336e588
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-03-29 00:46:36 +0300

    Add return data for Quiz component, add material UI components

--- quiz-client/src/components/Quiz.js
+++ quiz-client/src/components/Quiz.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { createAPIEndpoint, ENDPOINTS } from '../api';
-import useStateContext from '../hooks/useStateContext';
+import { Card, CardContent, CardMedia, CardHeader, List, ListItemButton, Typography, Box, LinearProgress } from '@mui/material'
 
 export default function Quiz() {
 
+  
   const [ qns, setQns ] = useState([]);
+  const [qnIndex, setQnIndex] = useState(0);
 
   useEffect(() => {
     createAPIEndpoint(ENDPOINTS.question)
@@ -17,6 +19,25 @@ export default function Quiz() {
   }, [])
 
   return (
-    <div>Question</div>
+    qns.length != 0
+      ? <Card>
+          <CardContent>
+            <Typography variant="h6">
+              {qns[qnIndex].qnInWords}
+            </Typography>
+            <List>
+                {qns[qnIndex].options.map((item, index) => 
+                  <ListItemButton key={index} disableRipple>
+                    <div>
+                      {item}
+                    </div>
+                  </ListItemButton>
+                )}
+              
+            </List>
+          </CardContent>
+      </Card>
+      : null
   )
+
 }
