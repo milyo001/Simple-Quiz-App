bc6feeaa547dad0e4e7ad2a35bc39c35fcd1362c
Miroslav Ilyovski <miroslavilyovski@abv.bg>
2022-03-30 16:47:55 +0300

    Add styles to 'MuiCardHeader-action'

--- quiz-client/src/components/Quiz.js
+++ quiz-client/src/components/Quiz.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { createAPIEndpoint, ENDPOINTS } from '../api';
 import { Card, CardContent, CardMedia, CardHeader, List, ListItemButton, Typography, Box, LinearProgress } from '@mui/material'
+import { getFormatedTime } from '../helper';
 
 export default function Quiz() {
 
@@ -35,17 +36,18 @@ export default function Quiz() {
   return (
     qns.length != 0
       ? <Card 
-        sx={{maxWidth: 640, mx:'auto', mt:5}}>
+        sx={{maxWidth: 640, mx:'auto', mt:5,
+              '& .MuiCardHeader-action': {m:0, alignSelf:'center'}}}>
           <CardContent>
             <CardHeader 
               title={'Question ' + (qnIndex + 1) + ' of ' + qns.length}
-              action={<Typography>{timeTaken}</Typography>}></CardHeader>
+              action={<Typography>{getFormatedTime(timeTaken)}</Typography>}></CardHeader>
             <Typography variant="h6">
               {qns[qnIndex].qnInWords}
             </Typography>
             <List>
                 {qns[qnIndex].options.map((item, index) => 
-                  <ListItemButton key={index} disableRipple>
+                  <ListItemButton key={index}>
                     <div>
                       {/* Get the ASCII code of letter A and increment it by the index of 
                         questions array,fetched from server */}
